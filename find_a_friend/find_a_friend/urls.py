187d34ea71b5baf710eddf1eb4719ac965562aa9
fcamas <fredy.camas-duy60@myhunter.cuny.edu>
2020-06-10 22:57:17 -0400

    added signup/login

--- find_a_friend/find_a_friend/urls.py
+++ find_a_friend/find_a_friend/urls.py
@@ -1,10 +1,10 @@
-
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from adoption  import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',views.adoption, name = 'adoption'),
+    path('accounts/', include('accounts.urls')),
 
 ]
