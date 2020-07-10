33ed0c9d08a7d5e471404d425492ac62e4554be9
matroskind <matroskind@gmail.com>
2020-07-10 13:39:41 -0400

    pushing changes made for production onto github

--- find_a_friend/accounts/models.py
+++ find_a_friend/accounts/models.py
@@ -4,21 +4,22 @@ from django.contrib.auth.models import AbstractUser
 
 
 class User(AbstractUser):
-    email = models.EmailField(unique=True, blank=False,
-                              error_messages={
-                                  'unique': "Email already exists.",
-                              })
-
-    USERNAME_FIELD = "email"
-    REQUIRED_FIELDS = []
-
-    def __str__(self):
-        return self.email
-
-    def get_full_name(self):
-        return self.email
-
-    def get_short_name(self):
-        return self.email
-
-    objects = UserManager()
+    pass
+    # email = models.EmailField(unique=True, blank=False,
+    #                           error_messages={
+    #                               'unique': "Email already exists.",
+    #                           })
+    #
+    # USERNAME_FIELD = "email"
+    # REQUIRED_FIELDS = []
+    #
+    # def __str__(self):
+    #     return self.email
+    #
+    # def get_full_name(self):
+    #     return self.email
+    #
+    # def get_short_name(self):
+    #     return self.email
+    #
+    # objects = UserManager()
--- find_a_friend/find_a_friend/settings.py
+++ find_a_friend/find_a_friend/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
-
+from decouple import config
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -20,12 +20,12 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'ol%e4c31$__80^30ua@^$bawc$fqs9n4$(o5_0q$3gqj_el*w7'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG')
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['find-afriend.herokuapp.com']
 
 
 # Application definition
@@ -79,8 +79,13 @@ WSGI_APPLICATION = 'find_a_friend.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'find_a_friend',
+        'USER': 'postgres',
+        'PASSWORD': config('DB_PASSWORD'),
+        'HOST': 'find-afriend.herokuapp.comgi', #
+        'PORT': '5432'
+
     }
 }
 
